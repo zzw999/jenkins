37b8d9fe9731ce9f9e4709372813e0040391d285
zzw999 <1372079788@qq.com>
2018-07-24 17:29:53 +0800

    jenkins测试

--- src/main/java/com/zzw/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/zzw/jenkinstest/JenkinsTestApplication.java
@@ -2,11 +2,20 @@ package com.zzw.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
     }
 }

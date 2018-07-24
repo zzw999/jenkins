91982cf2df700e2781ce53d645d04d4e71f81ea8
zzw999 <1372079788@qq.com>
2018-07-24 15:58:16 +0800

    jenkins测试

---
+++ src/main/java/com/zzw/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,12 @@
+package com.zzw.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+}
